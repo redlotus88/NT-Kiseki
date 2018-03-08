9ba03c824f82778ba38386e22ecff48b300f38b6
DragonWang <185138868@qq.com>
2018-03-08 17:34:26 +0800

    支持swagger 文档.

--- nt-eureka-client-example/src/main/java/cn/stsniper/eureka/example/ClientExampleApplication.java
+++ nt-eureka-client-example/src/main/java/cn/stsniper/eureka/example/ClientExampleApplication.java
@@ -3,12 +3,14 @@ package cn.stsniper.eureka.example;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by dragon on 2018/1/30.
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
 public class ClientExampleApplication {
 
     public static void main(String[] args) {
--- nt-eureka-client-example/src/main/java/cn/stsniper/eureka/example/DcController.java
+++ nt-eureka-client-example/src/main/java/cn/stsniper/eureka/example/DcController.java
@@ -1,5 +1,8 @@
 package cn.stsniper.eureka.example;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * PiW eureka client example
  */
+@Api(tags="Eureka Client", description = "DC Eureka测试类")
 @RestController
 public class DcController {
 
     @Autowired
     DiscoveryClient discoveryClient;
 
+    @ApiOperation(value = "测试客户端连接", notes = "")
     @GetMapping("/dc")
     public String dc() {
         String services = "Services: " + discoveryClient.getServices();
